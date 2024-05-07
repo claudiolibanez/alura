36e50690302c43c6046046c47f0e18368554b4cf
Claudio Soares <claudiolibanez@gmail.com>
2024-05-07 18:38:23 -0300

    add formacao java spring boot

---
+++ formacao-java/03-formacao-java-e-spring-boot/03-spring-boot-3-documente-teste-e-prepare-uma-api-para-o-deploy/api/src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -0,0 +1,45 @@
+package med.voll.api.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import med.voll.api.domain.ValidacaoException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+
+        return  ResponseEntity.badRequest().body(
+                errors.stream().map(DadosErroValidation::new).toList()
+        );
+    }
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity tratarErroRegraDeNegocio(ValidacaoException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    private record DadosErroValidation(
+            String campo,
+            String mensagem
+    ) {
+        public DadosErroValidation(FieldError erro) {
+            this(
+                    erro.getField(),
+                    erro.getDefaultMessage()
+            );
+        }
+    }
+
+}
